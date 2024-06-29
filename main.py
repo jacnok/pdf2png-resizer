07cc6c67613c1b77e6e51387f5badb907f0450f9
Jefferson C <jcherton@hotmail.com>
2024-06-29 17:52:21 -0500

    added more color

--- main.py
+++ main.py
@@ -59,22 +59,28 @@ def run_conversion():
 root = tk.Tk()
 root.title("PDF+ to PNG Converter")
 
+bg_color = '#038C5A'
+fg_color = '#F7F3EE'
+
+
+root.configure(background=bg_color)
+
 
 selected_files = tk.StringVar(value=preferences.get('pdf_file', ''))
 # pdf_path = tk.StringVar(value=preferences.get('pdf_file', ''))
 placeholder_path = tk.StringVar(value=preferences.get('placeholder_path', ''))
 output_folder = tk.StringVar(value=preferences.get('output_directory', ''))
 
-tk.Label(root, text="Select file:").grid(row=0, column=0, padx=10, pady=5)
-tk.Entry(root, textvariable=selected_files, width=50).grid(row=0, column=1, padx=10, pady=5)
+tk.Label(root, text="Select file:", bg=bg_color, fg=fg_color).grid(row=0, column=0, padx=10, pady=5)
+tk.Entry(root, textvariable=selected_files, width=100, justify=tk.RIGHT).grid(row=0, column=1, padx=10, pady=5)
 tk.Button(root, text="Browse", command=select_pdf).grid(row=0, column=2, padx=10, pady=5)
 
-tk.Label(root, text="Select Placeholder Image:").grid(row=1, column=0, padx=10, pady=5)
-tk.Entry(root, textvariable=placeholder_path, width=50).grid(row=1, column=1, padx=10, pady=5)
+tk.Label(root, text="Select Placeholder Image:",bg=bg_color, fg=fg_color).grid(row=1, column=0, padx=10, pady=5)
+tk.Entry(root, textvariable=placeholder_path, width=100, justify=tk.RIGHT).grid(row=1, column=1, padx=10, pady=5)
 tk.Button(root, text="Browse", command=select_placeholder).grid(row=1, column=2, padx=10, pady=5)
 
-tk.Label(root, text="Select Output Folder:").grid(row=2, column=0, padx=10, pady=5)
-tk.Entry(root, textvariable=output_folder, width=50).grid(row=2, column=1, padx=10, pady=5)
+tk.Label(root, text="Select Output Folder:", bg=bg_color, fg=fg_color).grid(row=2, column=0, padx=10, pady=5)
+tk.Entry(root, textvariable=output_folder, width=100, justify=tk.RIGHT).grid(row=2, column=1, padx=10, pady=5)
 tk.Button(root, text="Browse", command=select_output_folder).grid(row=2, column=2, padx=10, pady=5)
 
 tk.Button(root, text="Convert", command=run_conversion).grid(row=3, column=0, columnspan=3, pady=20)
